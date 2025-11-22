200987d914b93d0932edc71990684ff97aeebc86
bala252 <80048629+bala252@users.noreply.github.com>
2025-11-22 16:10:32 +0530

    new changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './first';
+function App() {
+  return (
+    
+    <div className="App">
+      <First />
+      <header className="App-header">
+       
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/First.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function First() {
+  return (
+    <div>
+      <h1>Hello, React! 👋</h1>
+      <p>Welcome to your first component.</p>
+    </div>
+  );
+}
+
+export default First;
